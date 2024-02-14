4ce3a1a71262888a19b9ae120bb3cc9ec85e1a02
Shubham Muddamwar <101393424+Shubham18598@users.noreply.github.com>
2024-02-14 13:50:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import { fileURLToPath } from "url"
 
 // Get directory name using import.meta.url
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = port.dirname(__filename);
 
 //configure env
 dotenv.config()
