7af4137360ce1aaaa69d52d622871f3bc8263ad5
Shubham18598 <muddamwarshubham98@gmail.com>
2024-06-08 11:50:22 +0530

    carosal add

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react"
 import Layout from "../components/Layout"
 import axios from "axios"
 import toast from "react-hot-toast"
-import { Checkbox, Radio } from "antd"
+import { Carousel, Checkbox, Radio } from "antd"
 import { Prices } from "../components/Prices"
 import { useNavigate } from "react-router-dom"
 import { useCart } from "../context/cart"
@@ -127,8 +127,39 @@ const HomePage = () => {
 
   return (
     <Layout title={"MYSHOP"}>
-      <div className="container-fluid row mt-3 home-page">
-        <div className="col-md-3 filters">
+      
+      <div className="container-fluid row home-page">
+      <Carousel autoplay>
+        <div>
+          <img
+            className="home-carousel"
+            src="https://img.freepik.com/free-photo/full-shot-happy-people-with-sale-tag_23-2149220654.jpg?t=st=1717751641~exp=1717755241~hmac=bdbe7cb76c5aa0616113bc92b3c71d2cfa49363f6fed56ce5b6c38ec55bdf30e&w=1380"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="home-carousel"
+            src="https://ps.w.org/woo-product-carousel-slider-and-grid-ultimate/assets/banner-1544x500.jpg?rev=2812931"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="home-carousel"
+            src="https://img.freepik.com/free-vector/colorful-abstract-sale-instagram-post-collection_23-2148309459.jpg?w=1380&t=st=1717751487~exp=1717752087~hmac=352fb16689b2a7d262304c26cac604434801b58be6c6a49d75f231d1f7cbed5a"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="home-carousel"
+            src="https://img.freepik.com/free-photo/sale-with-special-discount-filming-tools_23-2150040390.jpg?t=st=1717751581~exp=1717755181~hmac=440aa37cd343124e1a2a4fd064861ba58dc0e70f3ba5d9d7a8e02225bba5545e&w=1380"
+            alt=""
+          />
+        </div>
+      </Carousel>
+        <div className="col-md-3 mt-5 filters">
           <h4 className="text-center">Filter By Category</h4>
           <div className="d-flex flex-column">
             {categories?.map((c) => (
@@ -161,7 +192,7 @@ const HomePage = () => {
             </button>
           </div>
         </div>
-        <div className="col-md-9">
+        <div className="col-md-9 mt-5">
           <h1 className="text-center">All Products</h1>
           <div className="d-flex flex-wrap">
             {products.map((p) => (
